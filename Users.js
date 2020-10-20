c693fc8b8b12823d8a5649415f9227993489cd6a
Addy Monger <AddyMonger@Addys-MacBook-Air.local>
2020-10-19 22:35:37 -0400

    10/19

---
+++ Users.js
@@ -0,0 +1,24 @@
+const users = [
+    {
+        id: 1,
+        name: 'John',
+        email: 'johm@comcast.net'
+    },
+    {
+        id: 2,
+        name: 'Amit',
+        email: 'amit@comcast.net'
+    },
+    {
+        id: 3,
+        name: 'Tim',
+        email: 'tim@comcast.net'
+    },
+    {
+        id: 4,
+        name: 'Angie',
+        email: 'angie@comcast.net'
+    },
+];
+
+module.exports = users;
